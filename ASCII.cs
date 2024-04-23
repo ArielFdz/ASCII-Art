446fd77f22847d959375f1edc8975b2e8ba299b2
ArielFdz <arieljr11@outlook.com>
2024-04-22 22:51:38 -0600

    first commit

---
+++ ASCII.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCII_Art
+{
+    internal class ASCII
+    {
+        private static string[] _ascciChars = { "#", "#", "@", "%", "=", "+", "*", ":", "-", ".", " " };
+
+        public static string ASCIIGenerator(Bitmap image)
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < image.Height; i++)
+            {
+                for (int j = 0; j < image.Width; j++)
+                {
+                    Color pixelColor = image.GetPixel(j, i);
+                    int grayValue = CalculateGrayValue(pixelColor);
+                    int index = (grayValue * 10) / 255;
+                    sb.Append(_ascciChars[index]);
+                }
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+
+        private static int CalculateGrayValue(Color color)
+        {
+            return (int)(0.2126 * color.R + 0.7152 * color.G + 0.0722 * color.B);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Drawing;
+using ASCII_Art;
+
+do
+{
+    string path;
+
+    do
+    {
+        Console.WriteLine("Por favor, proporciona la ruta completa de la imagen:");
+        path = Console.ReadLine().Trim();
+
+        if (string.IsNullOrWhiteSpace(path))
+        {
+            Console.WriteLine("La ruta de la imagen no puede estar vacía.");
+        }
+        else if (!IsValidImagePath(path))
+        {
+            Console.WriteLine("La ruta de la imagen no es válida.");
+        }
+    } while (string.IsNullOrWhiteSpace(path) || !IsValidImagePath(path));
+
+    var img = Image.FromFile(path);
+    var image = new Bitmap(img, 150, 200);
+
+    var asciiContent = ASCII.ASCIIGenerator(image);
+    Console.WriteLine(asciiContent);
+
+    Console.WriteLine("¿Desea generar otro dibujo? (S/N)");
+} while (Console.ReadLine().Trim().ToUpper() == "S");
+
+static bool IsValidImagePath(string path)
+{
+    try
+    {
+        using (var img = Image.FromFile(path))
+        {
+            return true;
+        }
+    }
+    catch
+    {
+        return false;
+    }
+}
